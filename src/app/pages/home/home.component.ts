d4c0abe814ccde1753536ee7f1e86b2537c858ab
abdallahwishah <84712712+abdallahwishah@users.noreply.github.com>
2023-03-26 22:25:33 +0300

    tm

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o/public_api';
 import { filter, map, Observable, tap } from 'rxjs';
@@ -13,44 +14,63 @@ import { HomeService } from './home.service';
 export class HomeComponent implements OnInit {
   LandingPageData$!: Observable<any>;
   Services$!: Observable<any>;
-  LastNews$!:Observable<any>;
-  mix$!:Observable<any>;
+  LastNews$!: Observable<any>;
+  mix$!: Observable<any>;
 
   Advertisements$!: Observable<any>;
   isEn = document.dir == 'ltr' ? true : false;
   avatar = environment?.avatar;
 
-  VideoDialog$!:Observable<any>;
-  MediaCenterServiceNews$!:Observable<any>;
-  MediaCenterService$!:Observable<any>;
-  MediaCenterService2$!:Observable<any>;
+  VideoDialog$!: Observable<any>;
+  MediaCenterServiceNews$!: Observable<any>;
+  MediaCenterService$!: Observable<any>;
+  MediaCenterService2$!: Observable<any>;
 
-
-  constructor(private _homeService: HomeService , private _mediaCenterService:MediaCenterService , private el: ElementRef) {}
-
-
-
-
+  constructor(
+    private _homeService: HomeService,
+    private _mediaCenterService: MediaCenterService,
+    private el: ElementRef,
+    private location: Location
+  ) {}
+  dominUrl: any;
   ngOnInit(): void {
-/*    ;
- */
-
-
+    this.dominUrl = 'http://' + location.host;
+    console.log('this.dominUrl', this.dominUrl);
+    /*    ;
+     */
 
     this._homeService.getSliderData();
     this._homeService.getAdvertisements();
     this._homeService.getMainInfo();
-    this.LandingPageData$ = this._homeService.Selector$('LandingPageInfo').pipe(map(value=>value));
-    this.Services$ = this._homeService.Selector$('Services');
-
-
-    this.LastNews$ = this._mediaCenterService.Selector$('MediaSectionsItems').pipe(
-      map((val) => {
-        return val?.filter((item: any) => {
-          return item.MediaSectionID === 1;
-        });
+    this.LandingPageData$ = this._homeService.Selector$('LandingPageInfo').pipe(
+      map((value) => {
+        return {
+          ...value,
+          HeaderSlider: value?.HeaderSlider?.map((item: any) => {
+            console.log('this.dominUrl', this.dominUrl);
+            console.log(
+              'item',
+              item?.Link.toString()?.replace(this.dominUrl, '')
+            );
+            return {
+              ...item,
+              Link: item?.Link?.replace(this.dominUrl, ''),
+            };
+          }),
+        };
       })
     );
+    this.Services$ = this._homeService.Selector$('Services');
+
+    this.LastNews$ = this._mediaCenterService
+      .Selector$('MediaSectionsItems')
+      .pipe(
+        map((val) => {
+          return val?.filter((item: any) => {
+            return item.MediaSectionID === 1;
+          });
+        })
+      );
 
     this.mix$ = this._mediaCenterService.Selector$('MediaSectionsItems').pipe(
       map((val) => {
@@ -60,60 +80,64 @@ export class HomeComponent implements OnInit {
       })
     );
 
-/* ******************************************************************************************* */
-this.MediaCenterServiceNews$ = this._mediaCenterService.Selector$('MediaSectionsItems').pipe(
-  map((val) => {
-    return val?.filter((item: any) => {
-      return item.MediaSectionID === 1;
-    });
-  })
-);
+    /* ******************************************************************************************* */
+    this.MediaCenterServiceNews$ = this._mediaCenterService
+      .Selector$('MediaSectionsItems')
+      .pipe(
+        map((val) => {
+          return val?.filter((item: any) => {
+            return item.MediaSectionID === 1;
+          });
+        })
+      );
 
-    this.MediaCenterService$ = this._mediaCenterService.Selector$('MediaSectionsItems').pipe(
-      map((val) => {
-        return val?.filter((item: any) => {
-          return item.MediaSectionID === 3;
-        });
-      })
-    );
-    this.MediaCenterService2$ = this._mediaCenterService.Selector$('MediaSectionsItems').pipe(
-      map((val) => {
-        return val?.filter((item: any) => {
-          return item.MediaSectionID === 6;
-        });
-      })
-    );
+    this.MediaCenterService$ = this._mediaCenterService
+      .Selector$('MediaSectionsItems')
+      .pipe(
+        map((val) => {
+          return val?.filter((item: any) => {
+            return item.MediaSectionID === 3;
+          });
+        })
+      );
+    this.MediaCenterService2$ = this._mediaCenterService
+      .Selector$('MediaSectionsItems')
+      .pipe(
+        map((val) => {
+          return val?.filter((item: any) => {
+            return item.MediaSectionID === 6;
+          });
+        })
+      );
 
-/* **************************************************************************************** */
-
+    /* **************************************************************************************** */
   }
 
   /* ************************************************************************* */
 
   display: boolean = false;
 
-  showVideoPreview(item:any) {
+  showVideoPreview(item: any) {
     this.display = true;
     this._mediaCenterService.updateStore({ VideoDetails: item });
     console.log(item);
   }
 
-  showPhotosDetails(item:any){
+  showPhotosDetails(item: any) {
     this._mediaCenterService.updateStore({ PhotosDetails: item });
-    console.log(item)
+    console.log(item);
   }
 
   /* ************************************************************************* */
 
-  showNews(item:any){
+  showNews(item: any) {
     this._mediaCenterService.updateStore({ showNews: item });
-    console.log(item , "itemitemitem")
+    console.log(item, 'itemitemitem');
   }
-  showmix(item:any){
+  showmix(item: any) {
     this._mediaCenterService.updateStore({ MixDetails: item });
-    console.log(item)
+    console.log(item);
   }
-
 
   customOptions: OwlOptions = {
     loop: true,
@@ -146,7 +170,6 @@ this.MediaCenterServiceNews$ = this._mediaCenterService.Selector$('MediaSections
     },
   };
 
-
   mix: OwlOptions = {
     loop: true,
     rtl: true,
@@ -177,7 +200,4 @@ this.MediaCenterServiceNews$ = this._mediaCenterService.Selector$('MediaSections
       },
     },
   };
-
-
-
 }
